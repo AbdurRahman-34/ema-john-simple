9ecc730bc52981684e45b94c738e7fafa19fdc63
AbdurRahman-34 <get.abdurahman@gmail.com>
2022-11-22 19:21:39 +0600

    login authentication use

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,29 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 import './Login.css'
 
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+
+    const handelEmailBlur = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handelPasswordBlur = e => {
+        setPassword(e.target.value)
+    }
+
+    if(user){
+        navigate('/shop');
+        console.log("ok")
+    }
+
+    const handelUserSignIn = e => {
+        e.preventDefault()
+        signInWithEmailAndPassword(email, password)
+    }
+    
     return (
         <div className='form-container'>
            <div>
            <h2 className='form-titel'>Login</h2>
-            <form>
+            <form onSubmit={handelUserSignIn}>
                  
             <div className="input-group">
                 <label htmlFor="email"> Email</label>
-                <input type="email" name="email" placeholder='Enter Your Email 📧' id="" required/>
+                <input onBlur={handelEmailBlur} type="email" name="email" placeholder='Enter Your Email 📧' id="" required/>
             </div>
 
 
 
             <div className='input-group'>
                 <label htmlFor="password">Password</label>
-                <input type="password" name="" placeholder='Enter Your Password 🔒' id="" required/>
+                <input onBlur={handelPasswordBlur} type="password" name="" placeholder='Enter Your Password 🔒' id="" required/>
             </div>
 
-            
 
-            <input className='form-submit' type="submit" value="Login" />
+            <p style={{color: 'red'}}>{error?.message}</p>
+            {
+                loading && <p>Lodding .....</p>
+            }
+            <input onSubmit={handelUserSignIn} className='form-submit' type="submit" value="Login" />
             </form>
             <p>
                 New To Ema-John? <Link className='form-link' to='/signup'>Create a new account</Link>
